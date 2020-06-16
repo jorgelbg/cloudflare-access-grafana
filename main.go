94986c6db04e17f9a0c91e8c904fbf9fb7d1cf54
Jorge Luis Betancourt Gonzalez <jorge-luis.betancourt@trivago.com>
2020-06-16 02:02:03 +0200

    Add LICENSE.txt file and notice to the source code.
    Fatal in case that we cannot initialize the HTTP server

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Jorge Luis Betancourt <github@jorgelbg.me>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -98,5 +112,7 @@ func main() {
 	})))
 
 	log.Printf("Listening on http://%s/", listenAddr)
-	http.ListenAndServe(listenAddr, nil)
+	if err := http.ListenAndServe(listenAddr, nil); err != nil {
+		log.Fatalf("Unable to start server on [%s], error: %s", listenAddr, err.Error())
+	}
 }
